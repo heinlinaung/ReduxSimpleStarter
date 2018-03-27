80073b34ff5c14ba97146a7e9f9f536f9820d24a
Hein Lin Aung <heinlinaung@amdon.com>
2018-03-27 16:49:01 +0630

    Added : Youtube API and some fies changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyA8d8G_634lkxztX0uGDuEo8_jd1RboipU';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//Create a new component. It should produce html
+const App = () => {
+    return <div>Hi!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+// Take this component's generated html and put it on the page
+ReactDOM.render(<App />, document.querySelector('.container'));
