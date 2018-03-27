0f333ed2b2b93f70c1375843c133f28e75742a31
Hein Lin Aung <heinlinaung@amdon.com>
2018-03-27 17:30:55 +0630

    Change functional componet to class-based component in search_bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return <input />
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyA8d8G_634lkxztX0uGDuEo8_jd1RboipU';
 
 //Create a new component. It should produce html
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
 }
 
 
