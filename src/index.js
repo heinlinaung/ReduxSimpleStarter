07938afd65d5a3c3eec3ac04ba54e1f5078d612c
Hein Lin Aung <heinlinaung6891@gmail.com>
2018-03-28 22:51:25 +0630

    added controlled component in search_bar and use Youtube API search with default term

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyA8d8G_634lkxztX0uGDuEo8_jd1RboipU';
+
+YTSearch({ key:API_KEY, term :'dota2' },(data)=>{
+	console.log(data)
+})
 
 //Create a new component. It should produce html
 const App = () => {
