6573945b99239fbe86529d372f27fbc437beeb06
Hein Lin Aung <heinlinaung6891@gmail.com>
2018-03-28 23:55:35 +0630

    Refactored index.js and setState with default videos

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyA8d8G_634lkxztX0uGDuEo8_jd1RboipU';
 
-YTSearch({ key:API_KEY, term :'dota2' },(data)=>{
-	console.log(data)
-})
+class App extends Component  {
+	constructor(props){
+		super(props);
 
-//Create a new component. It should produce html
-const App = () => {
+		this.state = { videos : [] };
+
+		YTSearch({ key : API_KEY, term :'miracle- dota2' },(videos)=>{
+			this.setState({ videos })
+		})
+	}
+
+  render() {
     return (
-      <div>
-        <SearchBar />
-      </div>
-    );
+   		<div>
+    		<SearchBar />
+  		</div>
+  	);
+
+  }
 }
 
-
-// Take this component's generated html and put it on the page
 ReactDOM.render(<App />, document.querySelector('.container'));
