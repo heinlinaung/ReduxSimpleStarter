e1f1d676232690b403379012366f9b11486ed20e
Hein Lin Aung <heinlinaung6891@gmail.com>
2018-03-28 22:23:46 +0630

    Added state logic and binded onChange of input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,8 +3,17 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { term : '' };
+	}
   render() {
-    return <input onChange={e => console.log(e.target.value)} />
+    return (
+    	<div>
+    		<input onChange={e => this.setState({ term : e.target.value })} />
+  		</div>
+    )
   }
 }
 
